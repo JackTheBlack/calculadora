a95fc8acc07e44b7cf051ac7fd41df27a64987ff
JackTheBlack <ascona.nicolas@gmail.com>
2021-06-13 18:10:20 -0300

    s

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import './App.css';
 import Calculadora from './components/calculadora/index';
-import React,{Component} from "react";
+import React,{useState, Component} from "react";
 import Slider from './components/slider/index';
 
 
 
 const title = "Calculator";
+
+
 const slides=[
   {"title":"title1","text":"text1"},
   {"title":"title2","text":"text2"},
     {"title":"title3","text":"text3"},
 ]
 
+const names=["Ajay","Bobby","Chitra"];
+
+
+
 
 function App() {
 
 
+  
   return(<div>
-
+          
         <Calculadora/>
+       
     </div>);
-    <div>
-        <section>
-          <Slider/>
-        </section>
-    </div>
+    
 }
 
 export default App;
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -3,13 +3,19 @@ import React,{useState} from "react";
 
 
 
+function Slider(props){
 
-function Slider(slides){
+ 
+const [i,setI]=useState(0);
 
 
 return(
-  <div>
-    <h1>slides[0].title1</h1>
+ <div>
+      <button disabled={i==0} onClick={()=>setI(i-1)}>Previous</button>
+      <button  disabled={i==0} onClick={()=>setI(0)}>Restart</button>
+    <button disabled={i==2} onClick={()=>setI(i+1)}>Next</button>
+    <h1>{props.slides[i].title}</h1>
+    <h2>{props.slides[i].text}</h2>
   </div>
 );
 
