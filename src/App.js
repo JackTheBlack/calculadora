f2b8262c91c9550964453e8bba56d94648f82d39
JackTheBlack <ascona.nicolas@gmail.com>
2021-06-13 14:42:07 -0300

    slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Calculadora from './components/calculadora/index';
 import React,{Component} from "react";
+import Slider from './components/slider/index';
 
 
 
 const title = "Calculator";
+const slides=[
+  {"title":"title1","text":"text1"},
+  {"title":"title2","text":"text2"},
+    {"title":"title3","text":"text3"},
+]
+
 
 function App() {
 
@@ -15,7 +22,7 @@ function App() {
     </div>);
     <div>
         <section>
-          
+          <Slider/>
         </section>
     </div>
 }
---
+++ src/components/slider/index.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+
+
+
+
+
+function Slider(slides){
+
+
+return(
+  <div>
+    <h1>slides[0].title1</h1>
+  </div>
+);
+
+
+
+
+}
+export default Slider;
