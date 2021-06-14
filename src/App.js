baeeafef9866ed513a5b4a02f7248772dd88d42f
JackTheBlack <ascona.nicolas@gmail.com>
2021-06-13 21:44:25 -0300

    correcion

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
 import Calculadora from './components/calculadora/index';
 import React,{useState, Component} from "react";
-import Slider from './components/slider/index';
 
-
-
-const title = "Calculator";
-
-
-const slides=[
-  {"title":"title1","text":"text1"},
-  {"title":"title2","text":"text2"},
-    {"title":"title3","text":"text3"},
-]
-
-const names=["Ajay","Bobby","Chitra"];
 
 
 
@@ -22,13 +9,13 @@ const names=["Ajay","Bobby","Chitra"];
 function App() {
 
 
-  
+
   return(<div>
-          
+
         <Calculadora/>
-       
+
     </div>);
-    
+
 }
 
 export default App;
